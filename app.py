6d0a47a7e36de65bd9fd2f1fe906a715c2469694
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2024-09-17 00:26:13 +0530

    main

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port =443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
 
 
